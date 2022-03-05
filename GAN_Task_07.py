b41a03a62a425c83b71e218549639ad3ce1cf78e
Alk0gol3nok <65295678+Alk0gol3nok@users.noreply.github.com>
2022-03-05 15:34:31 +0300

    update  code GAN_Task_07

--- GAN_Task_07.py
+++ GAN_Task_07.py
@@ -1,13 +1,20 @@
 import random
+x = [i for i in range(10)]
+print(x)
+random.shuffle(x)
+print(x)
+random.shuffle(x)
+print(x)
 
-my_array = []
 
-count_array = int(input('Введите размер массива: '))
-for i in range(1, count_array+1):
-    my_array.append(random.randint(10, 100))
-print('Заполненный массив:', my_array)
-
-random.shuffle(my_array)
-print('Перемешанный список:(1 перемешка)', my_array)
-random.shuffle(my_array)
-print('Перемешанный список:(2 перемешка)', my_array)
+def shuffle(self, x, random=None):
+    if random is None:
+        randbelow = self._randbelow
+        for i in reversed(range(1, len(x))):
+            j = randbelow(i + 1)
+            x[i], x[j] = x[j], x[i]
+    else:
+        _int = int
+        for i in reversed(range(1, len(x))):
+            j = _int(random() * (i + 1))
+            x[i], x[j] = x[j], x[i]
